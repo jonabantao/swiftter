40f6e09150c1b6d4327878ebb22268dfe15d12fc
Jonathan Abantao <jonabantao@gmail.com>
2019-11-06 22:07:12 -0800

    Add placeholder and character count

--- Twitter/Controllers/TweetViewController.swift
+++ Twitter/Controllers/TweetViewController.swift
@@ -7,33 +7,55 @@
 //
 
 import UIKit
+import RSKPlaceholderTextView
 
-class TweetViewController: UIViewController {
-
-    @IBOutlet weak var tweetTextView: UITextView!
+class TweetViewController: UIViewController, UITextViewDelegate {
+    
+    private let maxCharacterLimit = 140
+    private var currentCharacterCount = 0 {
+        didSet {
+            let charactersRemaining = maxCharacterLimit - currentCharacterCount
+            
+            characterCountLabel.text = "\(charactersRemaining)"
+        }
+    }
+    @IBOutlet weak var tweetTextView: RSKPlaceholderTextView!
+    @IBOutlet weak var characterCountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tweetTextView.becomeFirstResponder()
+        self.tweetTextView.placeholder = "What's happpening?"
+        self.tweetTextView.delegate = self
+        
+        self.tweetTextView.becomeFirstResponder()
     }
     
-    @IBAction func onCancel(_ sender: Any) {
+    
+    @IBAction private func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func onTweet(_ sender: Any) {
+    @IBAction private func tweet(_ sender: Any) {
         if tweetTextView.text.isEmpty {
             return
         }
-        
+
         let tweetText = tweetTextView.text
         TwitterAPICaller.client?.postTweet(tweet: tweetText!, success: {
             self.dismiss(animated: true, completion: nil)
         }, failure: { (Error) in
             print("Error: \(Error)")
-            
+
             self.dismiss(animated: true, completion: nil)
         })
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        return textView.text.count < maxCharacterLimit
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        currentCharacterCount = textView.text.count
     }
 }
